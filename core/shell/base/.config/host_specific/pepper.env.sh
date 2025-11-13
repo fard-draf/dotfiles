98d951e77b288a3bde0140c53f6606e32952bafb
fard-draf <fard@ik.me>
2025-11-13 06:02:54 +0100

    update from pepper

--- core/shell/base/.config/host_specific/pepper.env.sh
+++ core/shell/base/.config/host_specific/pepper.env.sh
@@ -10,15 +10,15 @@ export KORRI="$DEV_PATH/warehouse/projects/professional/korrigan"
 #=======================================================
 
 #=======================================================HELIX
-export HELIX_RUNTIME=~/.local/helix/runtime
+export HELIX_RUNTIME=~/src/helix/runtime
 #=======================================================TYPESCRIPT
 export TYPESCRIPT_HOME="$DEV_PATH/tools/typescript"
 #=======================================================CARGO+RUST
 export CARGO_HOME="$DEV_PATH/tools/rust/cargo"
 export RUSTUP_HOME="$DEV_PATH/tools/rust/rustup"
-export RUST_ANALYZER_HOME="$DEV_PATH/tools/rust/rust-analyzer"
-#=======================================================NVM
-export NVM_DIR="$DEV_PATH/tools/nvm"
+export RUST_ANALYZER_HOME="$CARGO_HOME"
+#=======================================================NODEJS
+export NODEJS_HOME="$DEV_PATH/tools/nodejs"
 #=======================================================PYTHON
 export PYENV_ROOT="$DEV_PATH/tools/pyenv"
 #=======================================================YARN
