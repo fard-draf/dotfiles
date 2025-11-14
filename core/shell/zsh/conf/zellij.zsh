5904228008ca79f798e2ea580f2ec6a7f3e4985a
fard <fard@ik.me>
2025-11-14 13:48:31 +0100

    update(ssh/config zaatar)
    .ssh/config
    - ajout de mgmt.ans
    - reorganisation et nettoyage

--- core/shell/zsh/conf/zellij.zsh
+++ core/shell/zsh/conf/zellij.zsh
@@ -3,7 +3,7 @@ alias z.dev='zellij --layout dev attach --create dev'
 alias z.run='zellij --layout run attach --create run'
 alias z.kbsp='zellij --layout kbsp attach --create kbsp'
 alias z.dotf='zellij --layout dotf attach --create dotf'
-
+alias z.mgmt='zellij --layout mgmt attach --create mgmt'
 
 alias zka='zellij kill-all-sessions -y'
 alias zkc='zellij kill-session'
