bb18143e4eddce4710655af7031cc85df2ab5cfc
fard-draf <fard@ik.me>
2025-12-05 06:36:52 +0100

    update(core): aliases
    add z.reset -> nettoie le cache zellij

--- core/shell/zsh/conf/zellij.zsh
+++ core/shell/zsh/conf/zellij.zsh
@@ -1,10 +1,11 @@
 # ======= ALIAS ZELLIJ ========
 alias zsm='zellij -l welcome'
-alias z.dev='zellij --layout dev attach --create dev'
+alias z.dev='zellij --layout /home/fard/dev/warehouse/configs/dotfiles/dev/zellij/.config/zellij/layouts/dev.kdl attach --create dev'
 alias z.run='zellij --layout run attach --create run'
 alias z.kbsp='zellij --layout kbsp attach --create kbsp'
 alias z.dotf='zellij --layout dotf attach --create dotf'
 alias z.mgmt='zellij --layout mgmt attach --create mgmt'
+alias z.reset='rm -rf ~/.cache/zellij'
 
 alias zka='zellij kill-all-sessions -y'
 alias zkc='zellij kill-session'
