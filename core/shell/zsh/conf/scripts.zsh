a538f6b650113ccd895724e25c1ab9343b668037
fard <fard@ik.me>
2025-12-11 06:59:08 +0100

    update(core): script path
    add bin

--- core/shell/zsh/conf/scripts.zsh
+++ core/shell/zsh/conf/scripts.zsh
@@ -1,3 +1,3 @@
-alias sdto="$DEV_PATH/warehouse/configs/dotfiles/scripts/tools/send-to"
+alias sdto="$DEV_PATH/warehouse/configs/dotfiles/scripts/tools/bin/send-to"
 export PATH="$DEV_PATH/warehouse/configs/dotfiles/desktop/dsk_scripts/bin:$PATH"
 export PATH="$DEV_PATH/warehouse/configs/dotfiles/scripts/tools/bin:$PATH"
